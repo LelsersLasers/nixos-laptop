34840bc59190048fdcb7f7e680d34bebc39dac86
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2026-01-04 17:20:42 -0800

    nix-ld

--- configuration.nix
+++ configuration.nix
@@ -43,6 +43,9 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
+  # nix-ld
+  programs.nix-ld.enable = true;
+
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
