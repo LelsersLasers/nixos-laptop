45b3bf0f14deff139d6888adc00c2f610b738205
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2026-01-04 18:16:30 -0800

    needs to be one scope less

--- home.nix
+++ home.nix
@@ -1,43 +1,35 @@
 { config, pkgs, ... }:
 
 {
-
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.backupFileExtension = "hm-backup";
-
-  home-manager.users."millankumar" = {
     home.username = "millankumar";
     home.homeDirectory = "/home/millankumar";
 
     programs.bash = {
-      enable = true;
-      shellAliases = {
-        ls = "eza -al";
-        snrsfi = "sudo nixos-rebuild switch --flake . --impure";
-      };
+        enable = true;
+        shellAliases = {
+            ls = "eza -al";
+            snrsfi = "sudo nixos-rebuild switch --flake . --impure";
+        };
     };
 
     programs.helix = import ./helix/config.nix;
     programs.alacritty = import ./alacritty/config.nix;
 
     programs.git = {
-      enable = true;
-      userName = "LelsersLasers";
-      userEmail = "millankumar@gmail.com";
+        enable = true;
+        userName = "LelsersLasers";
+        userEmail = "millankumar@gmail.com";
     };
     programs.gh = {
-      enable = true;
-      gitCredentialHelper = {
         enable = true;
-      };
+        gitCredentialHelper = {
+        enable = true;
+        };
     };
-
 
 
     home.stateVersion = "25.05";
 
     programs.home-manager.enable = true;
-  }; 
 }
 
