015055cb298ed4d39a418ed5bbd064b2497d04ad
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2026-01-08 15:26:23 -0800

    laptop power saver?

--- configuration.nix
+++ configuration.nix
@@ -30,6 +30,29 @@
     };
   };
 
+  # Laptop
+  # Thermald proactively prevents overheating on Intel CPUs and works well with other tools
+  services.thermald.enable = true;
+  services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 80;
+
+       # Optional helps save long term battery health
+       START_CHARGE_THRESH_BAT0 = 60; # 40 and below it starts to charge
+       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      };
+};
+
 
   # Caps lock = escape key
   services.xserver.xkb.options = "caps:escape";
