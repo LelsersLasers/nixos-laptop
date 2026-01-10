9a18913374662c24847699afc50edeeaedcb6ceb
LelsersLasers <millankumar@gmail.com>
2026-01-10 15:05:34 -0800

    Indianpolis / Purdue timezone

--- configuration.nix
+++ configuration.nix
@@ -85,7 +85,7 @@
   hardware.bluetooth.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
+  time.timeZone = "America/Indianapolis";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
