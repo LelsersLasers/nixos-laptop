60c0aec47f4485e7c89cc96c566dc7129ab31355
LelsersLasers <millankumar@gmail.com>
2026-05-24 15:59:26 -0400

    west coast best coast

--- configuration.nix
+++ configuration.nix
@@ -109,7 +109,7 @@
   hardware.bluetooth.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/Indianapolis";
+  time.timeZone = "America/Los_Angeles";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
